e36d0fecca8b06b85efb1157ca4e52eed17f9571
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2019-01-30 12:03:05 -0800

    Add permission check and get location on button click

--- app/src/main/java/com/joshuahalvorson/android_locationservices_mapmarker/MapsActivity.java
+++ app/src/main/java/com/joshuahalvorson/android_locationservices_mapmarker/MapsActivity.java
@@ -1,18 +1,32 @@
 package com.joshuahalvorson.android_locationservices_mapmarker;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.MailTo;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final String TAG = "MapsActivity";
+    public static final int ACCESS_LOCATION_REQUEST_CODE = 1;
 
     private GoogleMap mMap;
 
@@ -30,7 +44,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this);
 
+        findViewById(R.id.center_on_location_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(
+                        context, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(
+                            (Activity) context,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            ACCESS_LOCATION_REQUEST_CODE);
+
+                } else {
+                    Log.i(TAG, "Location permission already granted");
+                    getLocation(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            double lat = location.getLatitude();
+                            double lon = location.getLongitude();
+
+                            mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(lat, lon)));
+                        }
+                    });
+                }
+            }
+        });
     }
 
 
@@ -51,5 +92,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == ACCESS_LOCATION_REQUEST_CODE) {
+            if (permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Log.i(TAG, "permission was granted");
+
+                getLocation(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        double lat = location.getLatitude();
+                        double lon = location.getLongitude();
+
+                        mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(lat, lon)));
+                    }
+                });
+
+            }
+        }
+    }
+
+    private void getLocation(OnSuccessListener<Location> onSuccessListener) {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED
+                &&
+                ActivityCompat.checkSelfPermission(
+                        this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+
+        fusedLocationProvider
+                .getLastLocation()
+                .addOnSuccessListener(onSuccessListener);
     }
 }
