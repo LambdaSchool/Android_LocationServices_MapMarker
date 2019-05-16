ee885d2ffc43d06b1694890999989144f8c633bd
heiligbasil <basilief@yahoo.com>
2019-05-16 15:52:56 -0700

    Hooked up the center-on-current-location button

--- app/src/main/java/com/lambdaschool/android_locationservices_mapmarker/MapsActivity.java
+++ app/src/main/java/com/lambdaschool/android_locationservices_mapmarker/MapsActivity.java
@@ -1,13 +1,23 @@
 package com.lambdaschool.android_locationservices_mapmarker;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.KeyEvent;
+import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,39 +25,65 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final int LOCATION_REQUEST_CODE = 33;
-    private ImageButton imageButtonCenter;
-    private ImageButton imageButtonPin;
     private GoogleMap googleMap;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        context = this;
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
 
-        imageButtonCenter = findViewById(R.id.image_button_center);
-        imageButtonPin = findViewById(R.id.image_button_pin);
+        ((ImageButton) findViewById(R.id.image_button_center)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+                } else {
+                    getLocation();
+                }
+            }
+        });
+
+        ((ImageButton) findViewById(R.id.image_button_pin)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
+    private void getLocation() {
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Seattle, Washington, USA.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        FusedLocationProviderClient locationProviderClient = LocationServices.getFusedLocationProviderClient(context);
+        locationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())),2000,null);
+                }
+            }
+        });
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
@@ -55,14 +91,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Seattle and move the camera
         LatLng seattle = new LatLng(47.6, -122.3);
         this.googleMap.addMarker(new MarkerOptions().position(seattle).title("Marker in Seattle"));
-        this.googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(seattle, 5f), 1000, null);
+        this.googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(seattle, 5f));
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == LOCATION_REQUEST_CODE) {
             if (permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                //getLocation();
+                getLocation();
             }
         }
     }
@@ -74,7 +110,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 googleMap.animateCamera(CameraUpdateFactory.zoomIn());
                 break;
             case KeyEvent.KEYCODE_DPAD_DOWN:
-                googleMap.animateCamera((CameraUpdateFactory.zoomOut()));
+                googleMap.animateCamera(CameraUpdateFactory.zoomOut());
                 break;
         }
         return super.onKeyDown(keyCode, event);
