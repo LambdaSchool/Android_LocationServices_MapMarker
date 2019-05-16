0a6dfa3ce2b7cfcd56d2080210a4bcc4915767c9
heiligbasil <basilief@yahoo.com>
2019-05-16 14:09:04 -0700

    Changed the default map location to Seattle and zoom in half-way

--- app/src/main/java/com/lambdaschool/android_locationservices_mapmarker/MapsActivity.java
+++ app/src/main/java/com/lambdaschool/android_locationservices_mapmarker/MapsActivity.java
@@ -29,7 +29,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker near Seattle, Washington, USA.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -38,9 +38,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Seattle and move the camera
+        LatLng seattle = new LatLng(47.6, -122.3);
+        mMap.addMarker(new MarkerOptions().position(seattle).title("Marker in Seattle"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seattle,5f));
     }
 }
